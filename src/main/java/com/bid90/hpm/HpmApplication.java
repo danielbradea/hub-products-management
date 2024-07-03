3be09e3ea884c6ea2ab8e2cab6c69f3eabeb60be
daniel.bradea <daniel.bradea@ferendin.com>
2024-07-03 11:30:17 +0300

    first commit

---
+++ src/main/java/com/bid90/hpm/HpmApplication.java
@@ -0,0 +1,13 @@
+package com.bid90.hpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpmApplication.class, args);
+	}
+
+}
